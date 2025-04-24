96169522461a5f5bc5fc8c1cc200bcbec70c3154
Kimah20X <pamlydia7@gmail.com>
2025-04-24 12:10:21 +0100

    contribute commit

--- server.js
+++ server.js
@@ -7,3 +7,14 @@ const port = 3000;
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
+
+
+function getRandomElement(arr) {
+  const randomIndex = Math.floor(Math.random() * arr.length);
+  return arr[randomIndex];
+}
+
+app.get('/quote', (req, res) => {
+  const randomQuote = getRandomElement(quotes);
+  res.send(randomQuote);
+});
